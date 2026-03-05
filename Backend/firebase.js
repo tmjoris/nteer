da164c509368523c17cfdb606c75d1fac67c2f72
tmjoris <tangajoris@gmail.com>
2026-03-05 07:59:37 +0300

    set the backend

---
+++ Backend/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBpjZRRuZ105bucc7kZMEf6SeGMeRcC2K8",
+  authDomain: "nteervolunteers.firebaseapp.com",
+  projectId: "nteervolunteers",
+  storageBucket: "nteervolunteers.firebasestorage.app",
+  messagingSenderId: "255671907577",
+  appId: "1:255671907577:web:cb288d8dd8269f6379de28"
+};
+
+const app = initializeApp(firebaseConfig);
